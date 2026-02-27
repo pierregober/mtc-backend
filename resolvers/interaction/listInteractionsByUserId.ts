61a98306fed7595e0c2d44e24aa5ed9a0f9d0ba8
Pierre Gober <pierregober@gmail.com>
2026-02-26 18:05:24 -0800

    update the backend

---
+++ resolvers/interaction/listInteractionsByUserId.ts
@@ -0,0 +1,26 @@
+// Vendors
+import { query } from '@aws-appsync/utils/dynamodb'
+import { util } from '@aws-appsync/utils'
+
+// Types
+import type { Context } from '@aws-appsync/utils'
+
+export function request(ctx: Context) {
+  const { userId, filter, limit, nextToken, sortDirection } = ctx.args
+
+  return query({
+    index: 'byUserId',
+    query: { userId: { eq: userId } },
+    filter,
+    limit,
+    nextToken,
+    scanIndexForward: sortDirection !== 'DESC',
+  })
+}
+
+export function response(ctx: Context) {
+  if (ctx.error) {
+    util.error(ctx.error.message, ctx.error.type, 'listInteractionsByUserId')
+  }
+  return ctx.result
+}
